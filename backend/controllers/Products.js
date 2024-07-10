a31b3724d64833137e97dadcfdf2db0db08b6387
ayra-sa <rizkyaryas11@gmail.com>
2024-07-10 20:47:17 +0700

    update product controllers, updateProduct and deleteProduct

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -105,17 +105,81 @@ export const createProduct = async (req, res) => {
   }
 };
 
-// export const updateProduct = async (req, res) => {
-//     try {
-//         const product = await Product.findOne({
-//           where: {
-//             uuid: req.params.id,
-//           },
-//         });
-//         if (!product) return res.status(404).json({ msg: "Data not found" });
-        
-//         res.status(200).json(response);
-//       } catch (error) {
-//         res.status(500).json({ msg: error.message });
-//       }
-// }
+export const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!product) return res.status(404).json({ msg: "Data not found" });
+    const { name, price } = req.body;
+    if (req.role === "admin") {
+      await Product.update(
+        { name, price },
+        {
+          where: {
+            id: product.id,
+          },
+        }
+      );
+    } else {
+      if (req.userId !== product.userId)
+        return res.status(403).json({ msg: "Access forbidden" });
+      await Product.update(
+        { name, price },
+        {
+          where: {
+            [Op.and]: [
+              {
+                id: product.id,
+              },
+              {
+                userId: req.userId,
+              },
+            ],
+          },
+        }
+      );
+    }
+    res.status(200).json({ msg: "Product updated successfuly" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!product) return res.status(404).json({ msg: "Data not found" });
+    if (req.role === "admin") {
+      await Product.destroy({
+        where: {
+          id: product.id,
+        },
+      });
+    } else {
+      if (req.userId !== product.userId)
+        return res.status(403).json({ msg: "Access forbidden" });
+      await Product.destroy({
+        where: {
+          [Op.and]: [
+            {
+              id: product.id,
+            },
+            {
+              userId: req.userId,
+            },
+          ],
+        },
+      });
+    }
+    res.status(200).json({ msg: "Product deleted successfuly" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
